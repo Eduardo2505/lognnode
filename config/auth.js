5d72daab1b310b0f42ff00c208d96aeb146cbad7
Padilla <eduardopadillacz@gmail.com>
2019-08-21 19:34:28 -0400

    Update

--- config/auth.js
+++ config/auth.js
@@ -23,9 +23,9 @@ module.exports = {
     },
 
     'googleAuth' : {
-        'clientID'         : '768439847293-8g2t0v9s3840v8crj0g8jbniam7l12v5.apps.googleusercontent.com',
-        'clientSecret'     : 'm-Yo5DTS0ZUnnu8Pmbx0RugF',
-        'callbackURL'      : 'http://apilink.cl/app/'
+        'clientID'         : '329789439298-ep9so89fe0c9rkkhjaiddligb334bq01.apps.googleusercontent.com',
+        'clientSecret'     : 'YIFxAqUIYqUy6BD3OnChFo50',
+        'callbackURL'      : 'http://login.pvessy.com/successSignOn'
     }
 
 };
